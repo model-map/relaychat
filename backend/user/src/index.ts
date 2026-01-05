872522e7b9dd26d6c46056c966a468c27cd270aa
Peach-mac <118442144+model-map@users.noreply.github.com>
2026-01-05 18:02:19 +0530

    Redis setup

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import axios from "axios";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import { createClient } from "redis";
 
 dotenv.config();
 const app = express();
@@ -13,19 +14,32 @@ app.use(morganMiddleware);
 
 connectDb();
 
-// ROUTES
-app.get("/test/crypto", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://api2.binance.com/api/v3/ticker/24hr"
-    );
-    const tickerPrice = response.data;
-    res.json(tickerPrice);
-  } catch (err) {
-    logger.error(err);
-    res.status(500).send("Internal server error");
-  }
+// REDIS SETUP
+
+const client = createClient({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_URL,
+    port: parseInt(process.env.REDIS_PORT!),
+  },
 });
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+await client.connect().then(() => console.log(`Connected to Redis.`));
+
+// app.get("/test/crypto", async (req, res) => {
+//   try {
+//     const response = await axios.get(
+//       "https://api2.binance.com/api/v3/ticker/24hr"
+//     );
+//     const tickerPrice = response.data;
+//     res.json(tickerPrice);
+//   } catch (err) {
+//     logger.error(err);
+//     res.status(500).send("Internal server error");
+//   }
+// });
 
 // SERVER LISTENING
 
