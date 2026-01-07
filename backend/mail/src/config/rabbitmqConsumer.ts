db5c1dde067741058e7bfea9ba785ceda224c1a8
Peach-mac <118442144+model-map@users.noreply.github.com>
2026-01-07 11:34:55 +0530

    mail-service: RabbitMQ consumer setup

---
+++ backend/mail/src/config/rabbitmqConsumer.ts
@@ -0,0 +1,57 @@
+import amqp from "amqplib";
+import nodemailer, { Transporter } from "nodemailer";
+import dotenv from "dotenv";
+import logger from "../utils/logger.js";
+dotenv.config();
+
+const RabbitMQ_URL = {
+  protocol: "amqp",
+  hostname: process.env.RABBITMQ_HOST,
+  port: parseInt(process.env.RABBITMQ_PORT!),
+  username: process.env.RABBITMQ_USER,
+  password: process.env.RABBITMQ_PASSWORD,
+};
+
+export const startSendOtpConsumer = async () => {
+  try {
+    const connection = await amqp.connect(RabbitMQ_URL);
+    const channel = await connection.createChannel();
+
+    const queueName = "sent-otp";
+
+    await channel.assertQueue(queueName, { durable: true });
+
+    logger.info(
+      "RabbitMQ: mail-service consumer started. Listening for OTP emails."
+    );
+
+    channel.consume(queueName, async (msg) => {
+      if (msg) {
+        try {
+          const { to, subject, body } = JSON.parse(msg.content.toString());
+          const transporter: Transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            auth: {
+              user: process.env.USER,
+              pass: process.env.PASSWORD,
+            },
+          });
+          await transporter.sendMail({
+            from: "Relay Chat",
+            to,
+            subject,
+            text: body,
+          });
+
+          logger.info(`OTP mail sent to ${to}`);
+          channel.ack(msg);
+        } catch (error) {
+          logger.error("Failed to send OTP: ", error);
+        }
+      }
+    });
+  } catch (error) {
+    logger.error(`Failed to start rabbitMQ consumer: ${error}`);
+  }
+};
