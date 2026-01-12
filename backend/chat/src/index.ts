e5894eaa0e9ec5d3093088c59c1df8269f6ab392
Peach-mac <118442144+model-map@users.noreply.github.com>
2026-01-12 09:38:06 +0530

    (feat/chat)-setup Redis, TryCatch for routes

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -4,14 +4,14 @@ import { fileURLToPath } from "url";
 import path from "path";
 import morganMiddleware from "./middleware/morganMiddleware.js";
 import logger from "./utils/logger.js";
+import { createClient } from "redis";
+import TryCatch from "./utils/TryCatch.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
-
-app.get("/", (req, res) => res.send("Hello"));
 
 // express global middlewares for body parsing and static files assets
 app.use(express.json());
@@ -20,11 +20,68 @@ app.use(express.static(path.join(__dirname, "public")));
 // Global middleware for logging HTTP requests
 app.use(morganMiddleware);
 
-const PORT = Number(process.env.PORT) || 6001;
+// Redis setup
+const redisClient = createClient({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    keepAlive: true,
+    host: process.env.REDIS_URL,
+    port: parseInt(process.env.REDIS_PORT!),
+    reconnectStrategy(retries, cause) {
+      if (retries > 10) return new Error("Retry limit reached.");
+      return Math.min(retries * 100, 3000);
+    },
+  },
+});
+
+// sending a ping to redis every 10 seconds to keep it alive
+setInterval(async () => {
+  try {
+    const res = await redisClient.ping();
+  } catch (error) {
+    logger.error(error);
+  }
+}, 10000);
+
+redisClient.on("error", (err: any) => {
+  console.log("Redis Client Error", err);
+  logger.error(err);
+});
+await redisClient.connect().then(() => logger.info(`Connected to Redis.`));
+
+export { redisClient };
+
+// ROUTES
+const service = "chat";
+app.get(
+  "/",
+  TryCatch(async (req, res) => {
+    // Current server start time
+    const time =
+      (await redisClient.get(`${service}-service-startTime`)) ||
+      new Date(Date.now()).toISOString();
+
+    const uptime = Math.floor((Date.now() - Date.parse(time)) / 1000);
+    // response
+    res.status(200).json({
+      message: `${service} service running.`,
+      Started: `${time}`,
+      uptime: `${uptime} seconds`, //uptime in seconds
+    });
+  })
+);
+
+// Start server config
+const PORT = Number(process.env.PORT) || 6000;
 app.listen(PORT, (err) => {
   if (err) {
     logger.error("Failed to start server:", err);
     return;
   }
+  redisClient.set(
+    `${service}-service-startTime`,
+    new Date(Date.now()).toISOString()
+  );
   logger.info(`Server is running on: http://localhost:${PORT}`);
 });
---
+++ backend/chat/src/utils/TryCatch.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+import logger from "./logger.js";
+
+const TryCatch = (handler: RequestHandler): RequestHandler => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await handler(req, res, next);
+    } catch (error: any) {
+      logger.error(error);
+      res.status(500).json({ message: error.message });
+    }
+  };
+};
+
+export default TryCatch;
