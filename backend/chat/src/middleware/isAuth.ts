ff3df1eff515e13895c26950bc60ff42537a51ee
Peach-mac <118442144+model-map@users.noreply.github.com>
2026-01-12 12:56:17 +0530

    (feat/chat) Create isAuth middleware

---
+++ backend/chat/src/middleware/isAuth.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from "express";
+import logger from "../utils/logger.js";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import dotenv from "dotenv";
+import { Document } from "mongoose";
+dotenv.config();
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+}
+
+export interface AuthenticatedRequest extends Request {
+  user?: IUser;
+}
+const isAuth = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    // Getting auth header from req.headers
+    const authHeader = req.headers.authorization;
+
+    // If no auth header or it doesn't contain Bearer token
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401).json({ message: "Please login - no auth header" });
+      return;
+    }
+    const token = authHeader.split(" ")[1];
+    // decode token
+    const decodedToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+
+    // check if decoded token exists and contains user
+    if (!decodedToken || !decodedToken.user) {
+      res.status(401).json({ message: "Invalid JWT token" });
+      return;
+    }
+
+    // If all else is good, then set req.user as the user in token, and call next() fn
+    req.user = decodedToken.user;
+    next();
+  } catch (error) {
+    logger.error("");
+    res.status(401).json({ message: "Please login - JWT error" });
+  }
+};
+
+export default isAuth;
